71105069e6180a59ad7ef477a85f24e8bd0f91e2
Mardel <rajanh@hotmail.fr>
2019-01-26 03:57:31 +0100

    Player Controller

---
+++ GGJ2019/Assets/Resources/Player/Scripts/PlayerInputController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour
+{
+    int _playerNumber = 1;
+    PlayerInputs _playerInputs;
+
+	void Update ()
+    {
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Move_Horizontal_P" + _playerNumber), -Input.GetAxisRaw("Move_Vertical_P" + _playerNumber), 0);
+        Vector3 rotation = new Vector3(Input.GetAxisRaw("Rotate_Horizontal_P" + _playerNumber), -Input.GetAxisRaw("Rotate_Vertical_P" + _playerNumber), 0);
+        bool shoot = Input.GetAxisRaw("Shoot_P" + _playerNumber) != 0;
+        //Debug.Log(Input.GetAxisRaw("Shoot_P" + _playerNumber));
+        //Debug.Log("Movement " + movement + "  Rotation : " + rotation);
+
+        _playerInputs = new PlayerInputs()
+        {
+            Movement = movement,
+            Rotation = rotation,
+            Shoot = shoot
+        };
+	}
+
+    public PlayerInputs GetInputs()
+    {
+        return _playerInputs;
+    }
+}
+
+public struct PlayerInputs
+{
+    public Vector3 Movement;
+    public Vector3 Rotation;
+    public bool Shoot;
+}
---
+++ GGJ2019/Assets/Resources/Player/Scripts/PlayerMachine.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerInputController))]
+public class PlayerMachine : StateMachine
+{
+    public enum PlayerStates { Idle, Move}
+
+    private PlayerInputController _inputController;
+
+    [SerializeField]
+    private float _rotateSpeed = 1500;
+    [SerializeField]
+    private float _moveSpeed = 10;
+    [SerializeField]
+    private float _shootRate = 0.1f;
+    private float _lastShot;
+
+    private Vector3 _movementInput;
+    private Vector3 _rotationInput;
+	void Start ()
+    {
+        _inputController = transform.GetComponent<PlayerInputController>();
+        currentState = PlayerStates.Idle;
+	}
+	
+    protected override void EarlyGlobalSuperUpdate()
+    {
+        _movementInput = _inputController.GetInputs().Movement;
+        _rotationInput = _inputController.GetInputs().Rotation;
+    }
+
+    protected override void LateGlobalSuperUpdate()
+    {
+        if (_rotationInput != Vector3.zero)
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(Vector3.forward, _rotationInput), _rotateSpeed * Time.deltaTime);
+        if (_inputController.GetInputs().Shoot)
+            Shoot();
+    }
+
+    void Idle_EnterState()
+    {
+
+    }
+
+    void Idle_Update()
+    {
+        if (_movementInput != Vector3.zero)
+        {
+            currentState = PlayerStates.Move;
+            return;
+        }
+    }
+
+    void Idle_ExitState()
+    {
+
+    }
+
+    void Move_EnterState()
+    {
+
+    }
+
+    void Move_Update()
+    {
+        if (_movementInput == Vector3.zero)
+        {
+            currentState = PlayerStates.Idle;
+            return;
+        }
+        transform.position += _movementInput * _moveSpeed * Time.deltaTime;
+    }
+
+    void Move_ExitState()
+    {
+
+    }
+
+    void Shoot()
+    {
+        if (_lastShot + _shootRate < Time.time)
+        {
+            //Debug.Log("Shoot");
+            // Spawn Projectile
+            _lastShot = Time.time;
+        }
+    }
+}
