385ea1cee300039fe849f86bbc95a5fb15b0462f
Yakonai <yoann.rey@epitech.eu>
2019-01-26 20:57:01 +0100

    merge

--- GGJ2019/Assets/Spawner.cs
+++ GGJ2019/Assets/Spawner.cs
@@ -1,99 +1,37 @@
 ﻿using System.Collections;
+using System.Runtime.Serialization;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour {
+[System.Serializable]
+public struct waveInfos
+{
+    public int elicount;
+    public int wormcount;
+    public int lutincount;
+}
 
-    public enum SpawnState
-    {
-        SPAWNING,
-        WAITING,
-        COUNTING
-    };
+public class Spawner : MonoBehaviour
+{
 
-    [System.Serializable]
     public class Wave
     {
-        public string name;
-        public Transform enemy;
-        public int count;
-        public float rate;
+        public GameObject _EliMonsterPrefab;
+        public GameObject _WormMonsterPrefab;
+        public GameObject _LutinMonsterPrefab;
+        public float _rate;
+        public waveInfos infos;
     }
 
-    public Wave[] waves;
-    private int nextWave = 0;
+    public Wave[] _waves;
+    private int _nextWave = 0;
 
-    public float timeBeforeNext = 10f;
-    public float waveCountDown;
+    public float _timeBeforeNext = 10f;
+    public float _waveCountDown;
 
-    private float searchCountdown = 1f;
-
-    private SpawnState state = SpawnState.COUNTING;
-
-    void Start()
+    void start()
     {
-        waveCountDown = timeBeforeNext;
-    }
-    
-    void Update()
-    {
-        if (state == SpawnState.WAITING)
-        {
-            if (!EnemyIsAlive())
-            {
-                Debug.Log("Wave Completed");
-                return;
-            }
-            else
-                return;
-        }
 
-
-        if (waveCountDown <= 0)
-        {
-            if (state != SpawnState.SPAWNING)
-            {
-                StartCoroutine(SpawnWave(waves[nextWave]));
-            }
-        }
-        else
-        {
-            waveCountDown = waveCountDown - Time.deltaTime;
-        }
     }
 
-    bool EnemyIsAlive()
-    {
-        searchCountdown -= Time.deltaTime;
-        if (searchCountdown <= 0f)
-        {
-            searchCountdown = 1f;
-            if (GameObject.FindGameObjectWithTag("Enemy") == null)
-                return false;
-        }
-
-        return true;
-    }
-
-    IEnumerator SpawnWave(Wave _wave)
-    {
-        Debug.Log("Spawning Wave" + _wave.name);
-        state = SpawnState.SPAWNING;
-
-        for (int i = 0; i < _wave.count; i++)
-        {
-            SpawnEnemy(_wave.enemy);
-            yield return new WaitForSeconds(1f / _wave.rate);
-        }
-
-        state = SpawnState.WAITING;
-
-        yield break;
-    }
-
-    void SpawnEnemy(Transform _enemy)
-    {
-        Instantiate(_enemy, transform.position, transform.rotation);
-        Debug.Log("spanwing enemey: " + _enemy.name);
-    }
 }
