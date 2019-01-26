ce6f8bfa1271f404c132eb066eccd67152f9d685
Yakonai <yoann.rey@epitech.eu>
2019-01-26 18:00:33 +0100

    merge

---
+++ GGJ2019/Assets/Spawner.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public enum SpawnState
+    {
+        SPAWNING,
+        WAITING,
+        COUNTING
+    };
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float timeBeforeNext = 10f;
+    public float waveCountDown;
+
+    private float searchCountdown = 1f;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+    void Start()
+    {
+        waveCountDown = timeBeforeNext;
+    }
+    
+    void Update()
+    {
+        if (state == SpawnState.WAITING)
+        {
+            if (!EnemyIsAlive())
+            {
+                Debug.Log("Wave Completed");
+                return;
+            }
+            else
+                return;
+        }
+
+
+        if (waveCountDown <= 0)
+        {
+            if (state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+        else
+        {
+            waveCountDown = waveCountDown - Time.deltaTime;
+        }
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+        if (searchCountdown <= 0f)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+                return false;
+        }
+
+        return true;
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        Debug.Log("Spawning Wave" + _wave.name);
+        state = SpawnState.SPAWNING;
+
+        for (int i = 0; i < _wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        Instantiate(_enemy, transform.position, transform.rotation);
+        Debug.Log("spanwing enemey: " + _enemy.name);
+    }
+}
