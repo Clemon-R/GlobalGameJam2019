9ca9e6a0bba1a46f527d9adbcd4f3b77af15e930
Yakon-Aïe <yoann.rey@epitech.eu>
2019-01-26 04:04:50 +0100

    adding 50% fire properties

---
+++ GGJ2019/Assets/Resources/Scripts/Fire.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+    private int originalHealth = 100;
+    public int actualHealth;
+    public float actualRange;
+    private float originalRange;
+    bool win;
+
+    Light lt;
+
+    // Use this for initialization
+
+    public float newRange
+    {
+        get
+        {
+            return actualHealth;
+        }
+        set
+        {
+            if (actualHealth > 0)
+            {
+                actualRange = ((float)actualHealth * originalRange) / (float)originalHealth;
+            }
+            else
+                win = false;
+        }
+    }
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+        originalRange = lt.range;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        actualRange = ((float)actualHealth * originalRange) / (float)originalHealth;
+        Debug.Log(actualRange);
+        lt.range = actualRange;
+	}
+}
