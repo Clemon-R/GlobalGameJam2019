05ee4abbf102a764111516c7d24417ec7fa3fc3b
Clemon <raphael.goulmot@epitech.eu>
2019-01-26 05:06:45 +0100

    Addong IEntity

---
+++ GGJ2019/Assets/Resources/Entity/Common/Scripts/IEntity.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IEntity {
+    void Hit(GameObject target, string colorCode);
+    void TakeHit(GameObject caster, string colorCode);
+}
--- GGJ2019/Assets/Resources/Entity/Monster/Scripts/Monster.cs
+++ GGJ2019/Assets/Resources/Entity/Monster/Scripts/Monster.cs
@@ -2,18 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Monster : MonoBehaviour {
-    private string colorCode;
+public class Monster : MonoBehaviour, IEntity
+{
+    public GameObject tmpTarget;
+    private string _color;
+
+    public void Hit(GameObject target, string colorCode)
+    {
+        if (target == null)
+            return;
+        IEntity entity = target.GetComponent<IEntity>();
+        if (entity == null)
+            return;
+        Debug.Log("[" + name + "] - Attack: "+target.name);
+        entity.TakeHit(this.gameObject, _color);
+    }
+
+    public void TakeHit(GameObject caster, string colorCode)
+    {
+        if (caster == null)
+            return;
+        Debug.Log("[" + name + "] - Get attacked by: " + caster.name);
+    }
 
     //Constuct for the monster
     public void Start()
     {
         Debug.Log("[" + name + "] - Constructing....");
         var randomColors = GetRandomColors();
-        colorCode = Color.Mix(randomColors);
-        Color.ChangeGameObjectColor(this.gameObject, colorCode);
-        Debug.Log("[" + name + "] - Construct the gameobject with the color: " + colorCode);
+        _color = Color.Mix(randomColors);
+        Color.ChangeGameObjectColor(this.gameObject, _color);
+        Debug.Log("[" + name + "] - Construct the gameobject with the color: " + _color);
         Debug.Log("[" + name + "] - Constructed");
+        Hit(tmpTarget, _color);
     }
 
     private string[] GetRandomColors()
