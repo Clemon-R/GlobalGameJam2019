b336e9df13195b09287381b3665e82c2fb13d0f0
Yakonai <yoann.rey@epitech.eu>
2019-01-27 17:13:31 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ GGJ2019/Assets/Resources/World/Scripts/GameOverScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour {
+
+	// Use this for initialization
+	public void GameOver()
+    {
+        Debug.Log("Hey");
+        SceneManager.LoadScene("Game");
+    }
+}
